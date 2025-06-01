aa31a59974a03e34a0adfd458752cea25255a9af
Igor Santos <igrrsantos@gmail.com>
2025-06-01 20:20:09 -0300

    test ci

--- src/main/java/com/learnai/LearnaiApplication.java
+++ src/main/java/com/learnai/LearnaiApplication.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.learnai"})
 public class LearnaiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LearnaiApplication.class, args);
 	}
-
 }
