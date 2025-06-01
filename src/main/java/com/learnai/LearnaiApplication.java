dccaa265cb4cf05b950b4040c1313825fc9c469d
Igor Santos <igrrsantos@gmail.com>
2025-06-01 19:46:27 -0300

    test

--- src/main/java/com/learnai/LearnaiApplication.java
+++ src/main/java/com/learnai/LearnaiApplication.java
@@ -2,8 +2,10 @@ package com.learnai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.learnai"})
 public class LearnaiApplication {
 
 	public static void main(String[] args) {
